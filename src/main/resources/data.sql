de855d8afff148536ac563c1d4f160ec1a3aa6c3
LouisAugustine <nzlouis.com@gmail.com>
2023-04-18 21:00:31 +1200

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+insert into user values('10@gmail.com', 'RESEARCH', 'Louis', 'Augustine');
+insert into user values('20@gmail.com', 'RESEARCH', 'Tom', 'Smith');
+insert into user values('30@gmail.com', 'SALES', 'James', 'Hudson');
+insert into user values( '40@gmail.com','OPERATIONS', 'Peter', 'William');
+
---
+++ src/test/java/com/nzlouis/GamesApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nzlouis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApiApplicationTests {
+
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+
+}
